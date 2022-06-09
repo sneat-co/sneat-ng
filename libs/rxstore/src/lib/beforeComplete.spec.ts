4beb5146de000758022e16fce369b025a9edf144
Alexander Trakhimenok <alex@trakhimenok.com>
2022-06-09 22:52:51 +0100

    added @sneat/rxstore library

---
+++ libs/rxstore/src/lib/beforeComplete.spec.ts
@@ -0,0 +1,10 @@
+import { beforeComplete } from './beforeComplete';
+
+describe('beforeComplete', () => {
+	it('should be created', () => {
+		const operator = beforeComplete(() => {
+		});
+		expect(operator)
+			.toBeTruthy();
+	});
+});
